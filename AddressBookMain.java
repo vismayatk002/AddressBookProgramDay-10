f068a1a60f4156ab179ab01517ca96c7528d8ad9
vismayatk002 <vismayatk002@gmail.com>
2021-08-08 15:18:55 +0530

    Added contact details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,6 +2,8 @@ public class AddressBookMain{
 
     public static void main(String[] args){
 
-        System.out.println("Welcome to Address Book Program");
+        AddressBook address = new AddressBook();
+        address.setDetails();
+        address.printDetails();
     }
 }
